4dede0b86832c13ff08ad7132a99d13e3f049072
Alena Bulanova <kxzws54@gmail.com>
2021-10-01 14:56:35 +0300

    feat: solve extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,37 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+export default function repeater(str, options) {
+  // throw new NotImplementedError('Not implemented');
+  /*str и addition - строки, приводить к строкам
+  seperator и additionSeparator - строки
+  repeatTimes и additionxRepeatTimes - целые числа, могут быть null/undefined
+  обязательный параметр - str, seperator default '+', additionSeparator default '|'*/
+  let addArr = [],
+    addRes = "";
+  if (options.additionRepeatTimes !== undefined && options.addition !== undefined) {
+    for (let i = 0; i < options.additionRepeatTimes; i++) {
+      addArr.push(String(options.addition));
+    }
+    if (options.additionSeparator !== undefined) {
+      addRes = addArr.join(options.additionSeparator);
+    } else addRes = addArr.join("|");
+  }
+  if (options.additionRepeatTimes === undefined && options.addition !== undefined) addRes += options.addition;
+
+  let strArr = [],
+    strRes = "";
+  if (options.repeatTimes !== undefined) {
+    for (let i = 0; i < options.repeatTimes; i++) {
+      strArr.push(String(str) + addRes);
+    }
+    if (options.separator !== undefined) {
+      strRes = strArr.join(options.separator);
+    } else strRes = strArr.join("+");
+  }
+  if (options.repeatTimes === undefined) strRes += str + addRes;
+
+  return strRes;
+  // npm test -- test/extended-repeater.test.js 
   // remove line with error and write your code here
 }
